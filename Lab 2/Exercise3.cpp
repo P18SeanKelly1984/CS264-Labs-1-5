9bb45e45cf7bcfd937d13e63f3caf33c7503d57b
Sean Kelly <32263169+P18SeanKelly1984@users.noreply.github.com>
2018-01-03 21:06:16 +0000

    Update Exercise3.cpp
    Improved layout

--- Lab 2/Exercise3.cpp
+++ Lab 2/Exercise3.cpp
@@ -10,47 +10,54 @@
 
 using namespace std;
 
-int main(int argc, char** argv) {
-	
+int main(int argc, char** argv) 
+{	
 	int dice1;
 	int dice2;
 	int outcomes[] = {0,0,0,0,0,0,0,0,0,0,0};
 	
-int i, n, total_val;
+	int i, n, total_val;
 
    //system clock to seed random number generator
    time_t t;  
    
    //Initialises random number generator. srand means pseudo random
+   //unsigned numbers aka positive values
    srand((unsigned) time(&t));
    
-   cout<<" Number of times to roll dice? \n";
+   cout << " Number of times to roll dice? \n";
    
-   cin>>n;
+   cin >> n;
 
    //Print random numbers from 2 to 12	
    for( i = 0 ; i < n; i++ ) 
      {
     	//have two dice with 6 faces
-	    dice1=rand() % 6 + 1;
-	    dice2=rand() % 6 + 1; 
+    	//% 6 will produce a number between 1 and 6
+    	//rand produces a value between 0 and int max?
+	    dice1 = rand() % 6 + 1;
+	    dice2 = rand() % 6 + 1; 
 	    
 	    //add up the value of the two dice 
 	    total_val = dice1 + dice2;
 	     
 	    for(int j = 0; j < 11; j++)
-	     
-	    if(total_val == j + 2) outcomes[j]++;
-	    
+	    {
+		 if(total_val == (j + 2))
+			{
+				outcomes[j]++; 
+			}    	
+		}
+
 	 }   
 	
 	for(i = 0; i < 11; i++)
 	  {
-	  cout<< "Outcome total " << i + 2 << " occurred " << outcomes[i] << " times. ";
-	  cout<< "Or " << ((100.0/(double)n)*outcomes[i]) << " % of rolls \n";
+		  //+2 when dice combined total is a min of 2
+		  cout<< "Outcome total " << i + 2 << " occurred " << outcomes[i] << " times. ";
+		  cout<< "Or " << ((100.0/(double)n)*outcomes[i]) << " % of rolls \n";
 	  }
 	
-	return 0;
-	
+	return 0;	
 	
 }//end main
